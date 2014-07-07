0952be7e6aab006c9a0433941075817616cf0c0e
Angelina Fabbro <angelinafabbro@gmail.com>
2014-07-07 13:46:16 -0700

    starting point for generator

--- generator-mozdevtools-presentation/app/index.js
+++ generator-mozdevtools-presentation/app/index.js
@@ -1,54 +1,203 @@
 'use strict';
-var util = require('util');
-var path = require('path');
-var yeoman = require('yeoman-generator');
-var yosay = require('yosay');
-var chalk = require('chalk');
+
+var util = require('util'),
+  path = require('path'),
+  yeoman = require('yeoman-generator'),
+  chalk = require('chalk');
 
 
-var MozdevtoolsPresentationGenerator = yeoman.generators.Base.extend({
-  init: function () {
-    this.pkg = require('../package.json');
+var BespokeGenerator = module.exports = function BespokeGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
 
-    this.on('end', function () {
-      if (!this.options['skip-install']) {
-        this.installDependencies();
-      }
-    });
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(BespokeGenerator, yeoman.generators.NamedBase);
+
+// welcome message
+var welcome = "Hello, wizard!"
+
+var mandatoryPlugins = [
+  {
+    name: 'keys',
+    version: '~0.1.0'
   },
-
-  askFor: function () {
-    var done = this.async();
-
-    // Have Yeoman greet the user.
-    this.log(yosay('Greetings, wizard. Prepare yourself to participate in the creation of a fabulous Mozilla Developer Tools slide deck.'));
-
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
-
-      done();
-    }.bind(this));
-  },
-
-  app: function () {
-    this.mkdir('app');
-    this.mkdir('app/templates');
-
-    this.copy('_package.json', 'package.json');
-    this.copy('_bower.json', 'bower.json');
-  },
-
-  projectfiles: function () {
-    this.copy('editorconfig', '.editorconfig');
-    this.copy('jshintrc', '.jshintrc');
+  {
+    name: 'touch',
+    version: '~0.1.0'
   }
-});
+];
 
-module.exports = MozdevtoolsPresentationGenerator;
+var optionalPlugins = [
+  {
+    name: 'bullets',
+    message: 'Would you like bullet list support?',
+    version: '~0.2.0',
+    configValue: 'li, .bullet'
+  },
+  {
+    name: 'scale',
+    message: 'Would you like responsive slide scaling?',
+    version: '~0.2.0'
+  },
+  {
+    name: 'hash',
+    message: 'Would you like hash routing support?',
+    version: '~0.1.0'
+  },
+  {
+    name: 'progress',
+    message: 'Would you like an animated progress bar?',
+    version: '~0.1.0'
+  },
+  {
+    name: 'state',
+    message: 'Would you like to be able to change the entire presentation style based on the active slide?',
+    version: '~0.2.0'
+  },
+  {
+    name: 'forms',
+    message: 'Will your presentation include interactive form elements?',
+    version: '~0.1.0'
+  }
+];
+
+BespokeGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  console.log(welcome);
+
+  var prompts = [{
+      name: 'title',
+      message: 'What is the title of your presentation?',
+      default: 'Hello World'
+    }]
+    .concat(optionalPlugins.map(function(plugin) {
+      return {
+        type: 'confirm',
+        name: plugin.name,
+        message: plugin.message,
+        default: true
+      };
+    }))
+    .concat({
+      type: 'confirm',
+      name: 'syntax',
+      message: 'Would you like syntax highlighting?',
+      default: true
+    });
+
+  this.prompt(prompts, function (props) {
+    mandatoryPlugins.forEach(function(plugin) {
+      this[plugin.name] = true;
+    }.bind(this));
+
+    optionalPlugins.forEach(function(plugin) {
+      this[plugin.name] = props[plugin.name];
+    }.bind(this));
+
+    this.selectedPlugins = mandatoryPlugins.concat(optionalPlugins.filter(function(plugin) {
+      return props[plugin.name];
+    }));
+
+    this.syntax = props.syntax;
+    this.title = props.title;
+    this.shortName = this._.slugify(props.title);
+
+    cb();
+  }.bind(this));
+};
+
+BespokeGenerator.prototype.setupProjectFiles = function setupProjectFiles() {
+  this.template('Gruntfile.js', 'Gruntfile.js');
+  this.template('README.md', 'README.md');
+
+  this.copy('bowerrc', '.bowerrc');
+  this.copy('gitignore', '.gitignore');
+  this.copy('jshintrc', '.jshintrc');
+  this.copy('editorconfig', '.editorconfig');
+}
+
+BespokeGenerator.prototype.setupPackageJson = function setupPackageJson() {
+  var packageJson = {
+    'name': this.shortName + '-bespoke',
+    'version': '0.0.0',
+    'dependencies': {},
+    'devDependencies': {
+      'grunt': '~0.4.1',
+      'grunt-contrib-clean': '~0.4.0',
+      'grunt-contrib-copy': '~0.4.1',
+      'grunt-contrib-watch': '~0.5.1',
+      'grunt-contrib-jade': '~0.9.0',
+      'grunt-contrib-stylus': '~0.5.0',
+      'grunt-contrib-coffee': '~0.7.0',
+      'grunt-usemin': '~2.0.2',
+      'grunt-contrib-concat': '~0.3.0',
+      'grunt-contrib-cssmin': '~0.7.0',
+      'grunt-contrib-uglify': '~0.2.7',
+      'grunt-contrib-connect': '~0.3.0',
+      'grunt-open': '~0.2.1',
+      'grunt-concurrent': '~0.3.0',
+      'grunt-gh-pages': '~0.6.0',
+      'connect-livereload': '~0.2.0',
+      'matchdep': '~0.3.0'
+    },
+    'engines': {
+      'node': '>=0.8.0'
+    }
+  };
+  this.write('package.json', JSON.stringify(packageJson, null, 2));
+};
+
+BespokeGenerator.prototype.setupBowerJson = function setupBowerJson() {
+  var bowerJson = {
+    'name': this.shortName + '-bespoke',
+    'version': '0.0.0',
+    'dependencies': {
+      'bespoke.js': '~0.4.0'
+    }
+  };
+
+  this.selectedPlugins.forEach(function(plugin) {
+    bowerJson.dependencies['bespoke-' + plugin.name] = plugin.version;
+  }.bind(this));
+
+  if (this.syntax) bowerJson.dependencies['prism'] = 'gh-pages';
+
+  this.write('bower.json', JSON.stringify(bowerJson, null, 2));
+};
+
+BespokeGenerator.prototype.setupBowerComponentPaths = function setupBowerComponentPaths() {
+  this.bowerComponentPaths = ['bespoke.js/dist/bespoke.min.js'];
+
+  this.selectedPlugins.forEach(function(plugin) {
+    this.bowerComponentPaths.push('bespoke-' + plugin.name + '/dist/bespoke-' + plugin.name +'.min.js');
+  }.bind(this));
+
+  if (this.syntax) this.bowerComponentPaths.push('prism/prism.js');
+};
+
+BespokeGenerator.prototype.setupPlugins = function setupPlugins() {
+  var plugins = this.selectedPlugins.reduce(function(pluginsObj, plugin) {
+    pluginsObj[plugin.name] = plugin.configValue != null ? plugin.configValue : true;
+    return pluginsObj;
+  }.bind(this), {});
+
+  this.pluginsJson = JSON.stringify(plugins, null, 2)
+    .replace(/\"/g,"'") // Switch to single quotes
+    .replace(/\'([a-z0-9_$]+)\'(:)/gi, '$1$2') // Unquote object keys
+};
+
+BespokeGenerator.prototype.setupFiles = function setupFiles() {
+  this.mkdir('src');
+  this.mkdir('src/scripts');
+  this.mkdir('src/styles');
+
+  this.template('src/index.jade', 'src/index.jade');
+  this.template('src/scripts/main.js', 'src/scripts/main.js');
+  this.template('src/styles/main.styl', 'src/styles/main.styl');
+};
